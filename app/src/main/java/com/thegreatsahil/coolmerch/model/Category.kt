bc773b6d6d65400cf9993ec9e9110fcded9bca30
sahilkuthe <sahilkkuthe2004@gmail.com>
2023-07-23 12:02:14 +0530

    Initial commit and all data added

---
+++ app/src/main/java/com/thegreatsahil/coolmerch/model/Category.kt
@@ -0,0 +1,3 @@
+package com.thegreatsahil.coolmerch.model
+
+class Category (val title: String, val image: String)
---
+++ app/src/main/java/com/thegreatsahil/coolmerch/services/DataServices.kt
@@ -0,0 +1,45 @@
+package com.thegreatsahil.coolmerch.services
+
+import com.thegreatsahil.coolmerch.model.Category
+import com.thegreatsahil.coolmerch.model.Product
+
+
+object DataServices {
+
+    val Categories = listOf(
+        Category("SHIRTS", "shirtImage"),
+        Category("Hoodies", "hoodieImage"),
+        Category("Hats", "hatImage"),
+        Category("Digital", "digitalGoodsImage")
+
+    )
+
+    val shirts = listOf(
+        Product("Black Logo Tee", "599", "shirt1"),
+        Product("White Tee", "599", "shirt2"),
+        Product("Red Logo Tee", "599", "shirt3"),
+        Product("Black Hustle Tee", "699", "shirt4"),
+        Product("Black Studio Logo Tee", "599", "shirt5")
+        )
+
+
+
+    val hats = listOf(
+        Product("Logo Black hat ", "Rs599", "hat1"),
+        Product("Logo Black cap ", "Rs699", "hat2"),
+        Product("Logo White cap ", "Rs499", "hat3"),
+        Product("Logo DuaTone cap ", "Rs799", "hat4")
+    )
+
+    val hoodies = listOf(
+        Product("Logo Black Hoodie", "1199", "hoodie1"),
+        Product("Logo Red Hoodie", "1199", "hoodie2"),
+        Product("Devslopes Grey Hoodie", "1199", "hoodie3"),
+        Product("Devslopes Black Hoodie", "1199", "hoodie4"),
+
+    )
+
+
+
+
+}
